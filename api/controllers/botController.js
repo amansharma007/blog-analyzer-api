5431d43de14dfe5bc984d1248b388474641841b3
Aman Sharma <aman8696.56@gmail.com>
2019-10-11 14:53:18 +0530

    Fixed the problem for multiple post-sitemap.xml in yoast seo plugin section

--- api/controllers/botController.js
+++ api/controllers/botController.js
@@ -7,27 +7,57 @@ module.exports = {
         let urlParam = req.query.url;
         let url = (urlParam.charAt(urlParam.length - 1) === '/') ? urlParam.substring(0, urlParam.length - 1) : urlParam;
         let blogList = new Array();
-        let sitemap = await axios.get(url+'/sitemap.xml')
-        .then((response) => {
-            return JSON.parse(convert.xml2json(response.data, {compact: true, spaces: 4}));
-        })
-        .catch((err) => {
-            res.send(err)
-        })
-        
-        // res.json(sitemap);
-        if(sitemap._comment){
-            // Yoast SEO Plugin Sitemaps
-            
-            /* !!!!!! Solve the case in which you have more than one /post-sitemap urls. For e.g, https://lifehack.org */
-            let postSitemap = await axios.get(url+'/post-sitemap.xml')
+        let postSitemap;
+        let sitemap = await axios.get(url + '/sitemap.xml')
             .then((response) => {
-                return JSON.parse(convert.xml2json(response.data, {compact: true, spaces: 4}));
+                return JSON.parse(convert.xml2json(response.data, {
+                    compact: true,
+                    spaces: 4
+                }));
             })
             .catch((err) => {
                 res.send(err)
             })
-            blogList = postSitemap.urlset.url.map(elem => elem.loc._text);
+
+        if (sitemap._comment) {
+            let counter = 0;
+            // Yoast SEO Plugin Sitemaps
+            let mainSitemap = sitemap.sitemapindex.sitemap;
+            for (let x in mainSitemap) {
+                if (mainSitemap[x].loc._text.search("/post-sitemap") > 0) {
+                    counter++;
+                }
+            }
+
+            // Now there are two cases for the counter. First is when counter = 1 and the second is otherwise.
+            if (counter === 1) {
+                postSitemap = await axios.get(url + '/post-sitemap.xml')
+                    .then((response) => {
+                        return JSON.parse(convert.xml2json(response.data, {
+                            compact: true,
+                            spaces: 4
+                        }));
+                    })
+                    .catch((err) => {
+                        res.send(err)
+                    });
+                blogList = postSitemap.urlset.url.map(elem => elem.loc._text);
+            } else if(counter > 1) {
+                for(let x=1; x <= counter; x++){
+                    postSitemap = await axios.get(url + '/post-sitemap'+x+'.xml')
+                    .then((response) => {
+                        return JSON.parse(convert.xml2json(response.data, {
+                            compact: true,
+                            spaces: 4
+                        }));
+                    })
+                    .catch((err) => {
+                        res.send(err)
+                    });
+                    blogList = blogList.concat(postSitemap.urlset.url.map(elem => elem.loc._text));
+                }
+            }
+
         } else {
             // Other Sitemaps
             blogList = sitemap.urlset.url.map(elem => elem.loc._text);

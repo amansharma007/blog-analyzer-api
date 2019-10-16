11c74388087dcc88d8abd1cc8f35eaed76425526
Aman Sharma <aman8696.56@gmail.com>
2019-10-16 15:01:48 +0530

    Added cors

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 var port = process.env.PORT || 3010;
 
-
+app.use(cors());
 
 var routes = require('./routes/appRoutes'); //importing route
 routes(app); //register the route
